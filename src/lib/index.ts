88b2a054e6c82f0a32000b9d6797864dc5532e94
unloco <amxmodx.unloco@gmail.com>
2024-05-14 00:29:06 +0100

    add db

--- src/lib/index.ts
+++ src/lib/index.ts
@@ -1,1 +1,8 @@
 // place files you want to import through the `$lib` alias in this folder.
+
+import { drizzle } from 'drizzle-orm/postgres-js';
+import { migrate } from 'drizzle-orm/postgres-js/migrator';
+import postgres from 'postgres';
+// for query purposes
+// postgres db for testing
+export const db = postgres("postgres://postgres.zqdukdapfcuxfqcvoohu:MnnIyKLcaUL3HEDK@aws-0-eu-central-1.pooler.supabase.com:5432/postgres");
