fc29685c9a9b4087518284b5e95ea50040153206
unloco <amxmodx.unloco@gmail.com>
2024-05-14 01:15:14 +0100

    add repro using postgres-js

--- README.md
+++ README.md
@@ -1,1 +1,20 @@
 # cloudflare-error-1101
+
+# Run project
+```bash
+run build
+# or
+run build -- --watch
+
+wrangler pages dev .svelte-kit/cloudflare --port=5173
+```
+
+The error occurs on http://localhost:5173/db
+
+Source: ./src/routes/db/+page.server.ts
+
+```text
+Error: The script will never generate a response.
+at async Object.fetch (file:///Users/USER/Library/pnpm/global/5/.pnpm/miniflare@3.20240419.1/node_modules/miniflare/dist/src/workers/core/entry.worker.js:966:22)
+```
+
--- src/routes/db/+page.server.ts
+++ src/routes/db/+page.server.ts
@@ -1,10 +1,31 @@
 import type { PageServerLoad } from './$types';
-import { db } from '$lib/db';
+import { db, queryClient } from '$lib/db';
 import { sql } from 'drizzle-orm';
 
+// create a sample promise
+async function getUsers() {
+	// error 1101
+	// Error: The script will never generate a response.
+	// 	at async Object.fetch (file:///Users/USER/Library/pnpm/global/5/.pnpm/miniflare@3.20240419.1/node_modules/miniflare/dist/src/workers/core/entry.worker.js:966:22)
+	return queryClient`SELECT * FROM users`;
+	// I thought drizzle was causing the issue, but it seems to be the queryClient from postgres-js
+	// return db.execute(sql`SELECT * FROM users`);
+	// return new Promise((resolve, reject) => {
+	// 	setTimeout(() => {
+	// 		resolve([{
+	// 			id: 1,
+	// 			name: 'John Doe'
+	// 		}, {
+	// 			id: 2,
+	// 			name: 'Jane Doe'
+	// 		}]);
+	// 	}, 1000);
+	// });
+}
+
 export const load = (async (event) => {
-	const users = await db.execute(sql`SELECT * FROM users`);
+	const users = await getUsers();
 	return {
 		users
-	}
+	};
 }) satisfies PageServerLoad;
