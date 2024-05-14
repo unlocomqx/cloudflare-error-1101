475feed8f31ca3154c1106b4e74fdae6a4901081
unloco <amxmodx.unloco@gmail.com>
2024-05-14 01:24:46 +0100

    try deploy

--- src/lib/db.ts
+++ src/lib/db.ts
@@ -1,10 +1,18 @@
-import { drizzle } from 'drizzle-orm/postgres-js';
+import { drizzle } from 'drizzle-orm/node-postgres';
 import postgres from 'postgres';
+import pg from "pg";
+
+const { Client } = pg;
 
 // postgres db for testing
 export const queryClient = postgres('postgres://postgres.zqdukdapfcuxfqcvoohu:MnnIyKLcaUL3HEDK@aws-0-eu-central-1.pooler.supabase.com:5432/postgres', {
 	prepare: false,
 	debug: true,
+	keep_alive: 1,
 });
 
-export const db = drizzle(queryClient);
+const client = new Client({
+	connectionString: "postgres://postgres.zqdukdapfcuxfqcvoohu:MnnIyKLcaUL3HEDK@aws-0-eu-central-1.pooler.supabase.com:5432/postgres",
+});
+
+export const db = drizzle(client);
